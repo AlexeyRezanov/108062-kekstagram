01334f62dbc5113e3329f98aef54d17f3722dee2
AlexeyRezanov <Alexey_Rezanov22@mail.ru>
2017-02-22 00:34:05 +0500

    делегирование фильтров и переключение их с клавиатуры (#3)

--- js/form.js
+++ js/form.js
@@ -4,13 +4,13 @@ var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadSelectImage = document.querySelector('#upload-select-image');
 var filterImagePreview = document.querySelector('.filter-image-preview');
 var uploadFile = document.querySelector('#upload-file');
-var uploadFormCancel = document.querySelector('.upload-form-cancel');
+var buttonClose = document.querySelector('.upload-form-cancel');
 var uploadFilterForm = document.querySelector('#upload-filter');
-var uploadFilters = uploadFilterForm.elements['upload-filter'];
 var selectedFilterClass = 'filter-none';
 var buttonInc = uploadFilterForm.querySelector('.upload-resize-controls-button-inc');
 var buttonDec = uploadFilterForm.querySelector('.upload-resize-controls-button-dec');
 var resizeControlsValue = uploadFilterForm.querySelector('.upload-resize-controls-value');
+var filterDiv = uploadFilterForm.querySelector('.upload-filter-controls');
 
 var DEFAULT_SCALE_VALUE = 100;
 var MAX_SCALE = 100;
@@ -18,33 +18,71 @@ var MIN_SCALE = 25;
 var SCALE_STEP = 25;
 var scale = DEFAULT_SCALE_VALUE;
 
+var ESCAPE_KEY_CODE = 27;
+var ENTER_KEY_CODE = 13;
+
 resizeControlsValue.value = DEFAULT_SCALE_VALUE + '%';
 
-uploadFormCancel.addEventListener('click', onClose);
+filterDiv.addEventListener('click', onSelectFilter);
+filterDiv.addEventListener('keydown', onSelectFilterByEnter);
+buttonClose.addEventListener('click', onClose);
 uploadFile.addEventListener('change', onOpen);
 buttonInc.addEventListener('click', onIncScale);
 buttonDec.addEventListener('click', onDecScale);
-
-for (var i = 0; i < uploadFilters.length; i++) {
-  uploadFilters[i].addEventListener('click', onSelectFilter);
-}
+uploadOverlay.addEventListener('keydown', onCloseEsc);
 
 function resize() {
   resizeControlsValue.value = scale + '%';
   filterImagePreview.style.transform = 'scale(' + scale / 100 + ')';
 }
 
+function close() {
+  uploadOverlay.classList.add('invisible');
+  uploadSelectImage.classList.remove('invisible');
+}
+
+function selectFilter(element) {
+  filterImagePreview.classList.remove(selectedFilterClass);
+  selectedFilterClass = 'filter-' + element.value;
+  filterImagePreview.classList.add(selectedFilterClass);
+}
+
 function onSelectFilter(event) {
   var target = event.target;
-  filterImagePreview.classList.remove(selectedFilterClass);
-  selectedFilterClass = 'filter-' + target.value;
-  filterImagePreview.classList.add(selectedFilterClass);
+
+  if (target.name !== 'upload-filter') {
+    return;
+  }
+
+  selectFilter(target);
+}
+
+function onSelectFilterByEnter(event) {
+  if (event.keyCode !== ENTER_KEY_CODE) {
+    return;
+  }
+
+  var target = event.target;
+  var parent = target.parentNode;
+  var filter = parent.previousElementSibling;
+
+  if (filter.name !== 'upload-filter') {
+    return;
+  }
+
+  selectFilter(filter);
+  filter.checked = true;
+}
+
+function onCloseEsc(event) {
+  if (event.keyCode === ESCAPE_KEY_CODE) {
+    close();
+  }
 }
 
 function onClose(event) {
   event.preventDefault();
-  uploadOverlay.classList.add('invisible');
-  uploadSelectImage.classList.remove('invisible');
+  close();
 }
 
 function onOpen() {
